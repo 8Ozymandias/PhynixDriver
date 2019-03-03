28d604cf2dd8618db429abb261f081f0b3ee8420
Jordon13 <remaxtek@gmail.com>
2019-03-03 10:17:03 -0500

    new updates

--- app/src/main/java/com/thephynix/www/phynix/DashboardActivity.java
+++ app/src/main/java/com/thephynix/www/phynix/DashboardActivity.java
@@ -3,7 +3,9 @@ package com.thephynix.www.phynix;
 import android.Manifest;
 import android.app.AutomaticZenRule;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -31,6 +33,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -43,8 +46,18 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -158,6 +171,7 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
                     public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
                         for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
                            result.setText("Latitude: "+documentSnapshot.get("Latitude").toString()+"\nLongitude: "+documentSnapshot.get("Longitude")+"\nIsAccepted: "+documentSnapshot.get("Accepted\n\n"));
+
                         }
 
                         if(queryDocumentSnapshots == null || queryDocumentSnapshots.isEmpty()){
@@ -168,6 +182,7 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
                                         public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
                                             for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
                                                 result.setText("Rider And Driver Is In Dialouge");
+
                                             }
 
                                             if(queryDocumentSnapshots == null || queryDocumentSnapshots.isEmpty()){
@@ -215,8 +230,19 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
                             info.put("DriverLongitude", myLatLng.longitude);
                             info.put("Accepted", true);
                             info.put("DriverId", DriverId);
-                            mMap.addMarker(new MarkerOptions().position(myLatLng).title("Rider Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                            mMap.clear();
+                            mMap.addMarker(new MarkerOptions().position(myLatLng).title("Me").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(task.getResult().getDouble("Latitude"),task.getResult().getDouble("Longitude"))).title("Rider Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+                            String url = getDirectionsUrl(myLatLng, new LatLng(task.getResult().getDouble("Latitude"),task.getResult().getDouble("Longitude")));
+
+                            DownloadTask downloadTask = new DownloadTask();
+
+                            // Start downloading json data from Google Directions API
+                            downloadTask.execute(url);
                             db.collection("lane").document("user_lane").set(info, SetOptions.merge());
+
                         }else{
                             Toast.makeText(DashboardActivity.this, "Another Driver Already Accepted", Toast.LENGTH_SHORT).show();
                         }
@@ -234,5 +260,148 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
 
     }
 
+
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            String data = "";
+
+            try {
+                data = downloadUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+
+            parserTask.execute(result);
+
+        }
+    }
+
+
+    /**
+     * A class to parse the Google Places in JSON format
+     */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                routes = parser.parse(jObject);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points = new ArrayList<LatLng>();
+            ;
+            PolylineOptions lineOptions = new PolylineOptions();
+            ;
+            lineOptions.width(2);
+            lineOptions.color(Color.RED);
+            MarkerOptions markerOptions = new MarkerOptions();
+            // Traversing through all the routes
+            for (int i = 0; i < result.size(); i++) {
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+                // Fetching all the points in i-th route
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+                    points.add(position);
+                }
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+
+            }
+            // Drawing polyline in the Google Map for the i-th route
+            if (points.size() != 0) mMap.addPolyline(lineOptions);//to avoid crash
+        }
+    }
+
+    private String getDirectionsUrl(LatLng origin, LatLng dest) {
+
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+        String mode = "mode=driving";
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor + "&" + mode;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=AIzaSyAsx0MNNbHc4yoAApTX3GFQ9zsTm470tlA";
+
+
+        return url;
+    }
+
+    /**
+     * A method to download json data from url
+     */
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.connect();
+
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
 
     }
